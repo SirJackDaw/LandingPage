a5dd308ef064c9eb086b2875fb958c6f3854d95b
Dmitry Zarubin <dimaster22@gmail.com>
2020-03-31 01:19:26 +0500

    move css out of assets

--- Landing2/script.js
+++ Landing2/script.js
@@ -8,11 +8,12 @@ img.onload = () => {
     ctx.drawImage(img, 0, 0);
 
     var url = 'https://cors-anywhere.herokuapp.com/https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
-    sendRequest(url)
+    var resp = sendRequest(url)
     .then(response => response.json())
     .then(data => {
       console.log(data);
     });
+    console.log(resp['quoteText']);
 }
 function sendRequest(url)
 {
