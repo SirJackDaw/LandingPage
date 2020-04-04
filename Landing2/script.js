61b6a221d8f87562fbe1ff22a652445693a93019
Dmitry Zarubin <sirjackdaw@outlook.com>
2020-04-04 18:07:01 +0500

    *

--- Landing2/script.js
+++ Landing2/script.js
@@ -1,45 +1,59 @@
 var canvas = document.getElementById("canvas"),
-			    ctx = canvas.getContext('2d');
+  ctx = canvas.getContext("2d");
 var img = new Image();
-img.src = 'https://source.unsplash.com/random';
+img.src = "https://source.unsplash.com/random";
 img.onload = () => {
-    canvas.width = img.width;
-    canvas.height = img.height;
-    ctx.drawImage(img, 0, 0);
+  canvas.width = img.width;
+  canvas.height = img.height;
+  ctx.drawImage(img, 0, 0);
 
-    var url = 'https://cors-anywhere.herokuapp.com/https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
-    var resp = sendRequest(url)
-    .then(response => response.json())
-    .then(data => {
+  var url =
+    "https://cors-anywhere.herokuapp.com/https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json";
+  var resp = sendRequest(url)
+    .then((response) => response.json())
+    .then((data) => {
       console.log(data.quoteText);
       var text = data.quoteText;
-      var fontSize = canvas.width / 20
-      ctx.font = `${fontSize}px Do Hyeon, sans-serif`;
+      var fontSize = canvas.width / 20;
+      ctx.font = `${fontSize}px 'Righteous', cursive`;
       ctx.fillStyle = "red";
       ctx.textAlign = "center";
-      // ctx.fillText(`${data.quoteText.toUpperCase()}`, canvas.width / 2, canvas.height / 2);
-      wrapText(ctx, text, canvas.width / 2, canvas.height / 2, canvas.width / 2, fontSize)
+      wrapText(
+        ctx,
+        text,
+        canvas.width / 2,
+        canvas.height / 2,
+        canvas.width / 2,
+        fontSize
+      );
     });
+};
+
+const sendRequest = url => {
+  return fetch(url, { mode: "cors" });
 }
-function sendRequest(url)
-{
-    return fetch(url, { mode: "cors" });
-}
-function wrapText(context, text, marginLeft, marginTop, maxWidth, lineHeight)
-    {
-      var words = text.split(" ");
-      var countWords = words.length;
-      var line = "";
-      for (var n = 0; n < countWords; n++) {
-        var testLine = line + words[n] + " ";
-        var testWidth = context.measureText(testLine).width;
-        if (testWidth > maxWidth) {
-          context.fillText(line, marginLeft, marginTop);
-          line = words[n] + " ";
-          marginTop += lineHeight;
-        } else {
-          line = testLine;
-        }
-      }
+
+const wrapText = (context, text, marginLeft, marginTop, maxWidth, lineHeight) => {
+  var words = text.split(" ");
+  var countWords = words.length;
+  var line = "";
+  for (var n = 0; n < countWords; n++) {
+    var testLine = line + words[n] + " ";
+    var testWidth = context.measureText(testLine).width;
+    if (testWidth > maxWidth) {
       context.fillText(line, marginLeft, marginTop);
+      line = words[n] + " ";
+      marginTop += lineHeight;
+    } else {
+      line = testLine;
     }
+  }
+  context.fillText(line, marginLeft, marginTop);
+}
+
+const download_img = (el) => {
+var canvas = document.getElementById("canvas")
+  var image = canvas.toDataURL("image/png");
+  console.log(image);
+  el.href = image;
+};
